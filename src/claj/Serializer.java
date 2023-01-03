dea5d62fc1a1829db98bf7207ccc6ff7fc064666
xzxADIxzx <valerazhuk20122006@gmail.com>
2023-01-03 20:18:55 +0300

    remove mindustry core from dependencies

--- src/claj/Serializer.java
+++ src/claj/Serializer.java
@@ -1,22 +1,27 @@
 package claj;
 
-import mindustry.net.ArcNetProvider.PacketSerializer;
+import arc.net.FrameworkMessage;
+import arc.net.FrameworkMessage.*;
+import arc.net.NetSerializer;
 
 import java.nio.ByteBuffer;
 
-public class Serializer extends PacketSerializer {
+public class Serializer implements NetSerializer {
 
-    public static final byte linkID = -3;
-
+    public static final byte frameworkID = -2, linkID = -3;
     public ByteBuffer last = ByteBuffer.allocate(8192);
 
     @Override
     public void write(ByteBuffer buffer, Object object) {
-        if (object instanceof String link) {
+        if (object instanceof ByteBuffer raw) {
+            buffer.put(raw);
+        } else if (object instanceof FrameworkMessage message) {
+            buffer.put(frameworkID);
+            writeFramework(buffer, message);
+        } else if (object instanceof String link) {
             buffer.put(linkID);
             writeString(buffer, link);
-        } else
-            super.write(buffer, object);
+        }
     }
 
     @Override
@@ -24,14 +29,49 @@ public class Serializer extends PacketSerializer {
         int lastPosition = buffer.position();
         byte id = buffer.get();
 
-        if (id == -2) return super.readFramework(buffer);
-        if (id == linkID) return readString(buffer); // link
+        if (id == frameworkID) return readFramework(buffer);
+        if (id == linkID) return readString(buffer);
 
         last.clear();
         last.put(buffer.position(lastPosition));
         last.limit(buffer.limit() - lastPosition);
 
         return last.position(0);
+    }
+
+    public void writeFramework(ByteBuffer buffer, FrameworkMessage message) {
+        if (message instanceof Ping p)
+            buffer.put((byte) 0).putInt(p.id).put(p.isReply ? (byte) 1 : 0);
+        else if (message instanceof DiscoverHost)
+            buffer.put((byte) 1);
+        else if (message instanceof KeepAlive)
+            buffer.put((byte) 2);
+        else if (message instanceof RegisterUDP p)
+            buffer.put((byte) 3).putInt(p.connectionID);
+        else if (message instanceof RegisterTCP p)
+            buffer.put((byte) 4).putInt(p.connectionID);
+    }
+
+    public FrameworkMessage readFramework(ByteBuffer buffer) {
+        byte id = buffer.get();
+
+        if (id == 0)
+            return new Ping() {{
+                id = buffer.getInt();
+                isReply = buffer.get() == 1;
+            }};
+        else if (id == 1) return FrameworkMessage.discoverHost;
+        else if (id == 2) return FrameworkMessage.keepAlive;
+        else if (id == 3)
+            return new RegisterUDP() {{
+                connectionID = buffer.getInt();
+            }};
+        else if (id == 4)
+            return new RegisterTCP() {{
+                connectionID = buffer.getInt();
+            }};
+
+        throw new RuntimeException("Unknown framework message!");
     }
 
     public static void writeString(ByteBuffer buffer, String message) {
